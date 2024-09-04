9ea23fb6b9cccdb58698711667a0097d24c9bab0
unknown <mernaatef135@gmail.com>
2024-09-05 00:27:43 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+ $(document).ready(function (){
+
+ $('#loading .lds-dual-ring').fadeOut(1500,()=>{
+    $('#loading').fadeOut(1500,() => {
+      $('body').css('overflow-y','auto')
+    })
+   });
+ 
+  })
+
+
+
+
